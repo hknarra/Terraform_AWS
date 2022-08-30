6fde1234e27739b79902c5b1252079938c801fae
hknarra <hk.narra@gmail.com>
2022-08-30 18:55:05 +0530

    c3

--- 03_S3/16_s3bucket_static_website_hosting_with_reusable_modules/c3-s3bucket.tf
+++ 03_S3/16_s3bucket_static_website_hosting_with_reusable_modules/c3-s3bucket.tf
@@ -1,5 +1,4 @@
-# Call our Custom Terraform Module which we built earlier
-
+# Call Custom Terraform Module
 module "website_s3_bucket" {
   source      = "./modules/aws-s3-static-website-bucket" # Mandatory
   bucket_name = var.my_s3_bucket
