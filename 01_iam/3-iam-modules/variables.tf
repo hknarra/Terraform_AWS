80f286d7a1b96c526257abf46a3ac2c634684a7a
hknarra <hk.narra@gmail.com>
2022-09-08 19:05:09 +0530

    c3

---
+++ 01_iam/3-iam-modules/variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {
+    description = "aws region"
+    type = string
+    default = "us-east-1"
+  
+}
---
+++ 01_iam/3-iam-user-login-profile/1versions.tf
@@ -0,0 +1,17 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.26.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
+
